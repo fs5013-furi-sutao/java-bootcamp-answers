f22d253454559450ee1d61e258d841f0338115b3
fs5013-furi-sutao <fs5013.furi.sutao@gmail.com>
2020-12-10 12:51:04 +0900

    First commit

---
+++ 02.entry/01.variables/src/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        int fatherAge = 42;
+        int motherAge = 38;
+        int myAge = 20;
+        int myWeightKg = 670;
+
+        System.out.printf("ぼくのパパの年齢は %d です.%n", fatherAge);
+        System.out.printf("ぼくのママの年齢は %d です.%n", motherAge);
+        System.out.printf("ぼくの年齢は %d です.%n", myAge);
+        System.out.println();
+        System.out.printf("ぼくの体重は %dKg です.%n", myWeightKg);
+    }
+}
