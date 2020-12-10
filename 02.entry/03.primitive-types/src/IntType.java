72d0ce26d47db149953742efe15630410582f4a0
fs5013-furi-sutao <fs5013.furi.sutao@gmail.com>
2020-12-10 17:28:58 +0900

    Modified IntType.java

--- 02.entry/03.primitive-types/src/IntType.java
+++ 02.entry/03.primitive-types/src/IntType.java
@@ -2,7 +2,7 @@ public class IntType {
     public static void main(String[] args) {
 
         int minInt = Integer.MIN_VALUE;
-        int maxInt = Integer.MAX_VALUE + 1;
+        int maxInt = Integer.MAX_VALUE;
 
         System.out.printf("int 型の最小値 = %s%n", minInt);
         System.out.printf("int 型の最大値 = %s%n", maxInt);
