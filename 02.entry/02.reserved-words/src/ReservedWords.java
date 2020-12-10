addd174896a30f0f2b7ff90f5c3ccbd875e2622b
fs5013-furi-sutao <fs5013.furi.sutao@gmail.com>
2020-12-10 14:15:12 +0900

    Add 02.reserved-words

---
+++ 02.entry/02.reserved-words/src/ReservedWords.java
@@ -0,0 +1,12 @@
+public class ReservedWords {
+    public static void main(String[] args) {
+        
+        int this = 12;
+        int that = 23;
+        int default = 0;
+        
+        default = this + that;
+
+        System.out.printf("%d + %d = %d%n", this, that, default);
+    }
+}
