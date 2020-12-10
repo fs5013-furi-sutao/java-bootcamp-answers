36aa66b73066af62869064645e39a0d9d822960d
fs5013-furi-sutao <fs5013.furi.sutao@gmail.com>
2020-12-10 14:18:28 +0900

    Fix ReservedWords.java

--- 02.entry/02.reserved-words/src/ReservedWords.java
+++ 02.entry/02.reserved-words/src/ReservedWords.java
@@ -1,12 +1,12 @@
 public class ReservedWords {
     public static void main(String[] args) {
         
-        int this = 12;
-        int that = 23;
-        int default = 0;
-        
-        default = this + that;
+        int firstNum = 12;
+        int secondNum = 23;
+        int sum = 0;
 
-        System.out.printf("%d + %d = %d%n", this, that, default);
+        sum = firstNum + firstNum;
+
+        System.out.printf("%d + %d = %d%n", firstNum, secondNum, sum);
     }
 }
