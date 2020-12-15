cf8ea9ff94ce84b6eb8c6d8228ffbc3509ff7870
fs5013-furi-sutao <fs5013.furi.sutao@gmail.com>
2020-12-15 14:36:11 +0900

    Add article

---
+++ 02.entry/09.named-boolean-variables/src/SurvivorChecker.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class SurvivorChecker {
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+        String myName = "山本一郎";
+        boolean isLive = rand.nextBoolean();
+
+        if (isLive) {
+            System.out.printf("%s は生きてるよ.", myName, isLive);
+        } else {
+            System.out.printf("%s は死んでるよ.", myName, isLive);
+        }
+    }
+}
