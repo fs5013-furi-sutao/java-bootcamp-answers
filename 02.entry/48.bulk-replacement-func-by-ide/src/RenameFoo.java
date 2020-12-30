ec0e83a035072930694ba35225af9fef905f7723
fs5013-furi-sutao <fs5013.furi.sutao@gmail.com>
2020-12-31 03:29:56 +0900

    Add article

---
+++ 02.entry/48.bulk-replacement-func-by-ide/src/RenameFoo.java
@@ -0,0 +1,12 @@
+public class RenameFoo {
+
+    private String name;
+
+    public RenameFoo() {
+        this.name = "John Doe";
+    }
+
+    public void showName() {
+        System.out.println(this.name);
+    }
+}
---
+++ 02.entry/48.bulk-replacement-func-by-ide/src/RenameVerification.java
@@ -0,0 +1,8 @@
+public class RenameVerification {
+    public static void main(String[] args) {
+
+        RenameFoo obj = new RenameFoo();
+        
+        obj.showName();
+    }
+}
