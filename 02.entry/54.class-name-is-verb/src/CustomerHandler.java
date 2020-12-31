421d7b8ed044bfba3c8603c53c959a33cd054336
fs5013-furi-sutao <fs5013.furi.sutao@gmail.com>
2021-01-01 05:42:46 +0900

    Add article

---
+++ 02.entry/54.class-name-is-verb/src/CustomerHandler.java
@@ -0,0 +1,11 @@
+public class CustomerHandler {
+
+    public static void main(String[] args) {
+
+        Customer yamamoto = new Customer("山本一郎", 200);
+        Customer yamada = new Customer("山田太郎", 300);
+        yamamoto.pay();
+        yamamoto.showIsPaid();
+        yamada.showIsPaid();
+    }
+}
