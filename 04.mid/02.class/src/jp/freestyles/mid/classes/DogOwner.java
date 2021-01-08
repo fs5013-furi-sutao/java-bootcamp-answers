80ff2daef82025d3de997e6cbd7e2ed91e91563f
fs5013-furi-sutao <fs5013.furi.sutao@gmail.com>
2021-01-08 18:24:34 +0900

    Fix java files

--- 04.mid/02.class/src/jp/freestyles/mid/classes/DogOwner.java
+++ 04.mid/02.class/src/jp/freestyles/mid/classes/DogOwner.java
@@ -7,7 +7,7 @@ public class DogOwner {
     public static void main(String[] args) {
 
         List<Dog> dogs = new ArrayList<>();
-        dogs.add(new Dog("山形賢", 7, "秋田犬"));
+        dogs.add(new Dog("秋田賢", 7, "秋田犬"));
         dogs.add(new Dog("山本一郎", 1, "プードル"));
         dogs.add(new Dog("山田太朗", 2, "チワワ"));
 
