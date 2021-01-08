111cbe12867279493cdb5700720c60be5ba5e3d6
fs5013-furi-sutao <fs5013.furi.sutao@gmail.com>
2021-01-08 17:21:34 +0900

    Add article

---
+++ 04.mid/02.class/src/jp/freestyles/mid/classes/jp/freestyles/mid/classes/DogOwner.java
@@ -0,0 +1,21 @@
+package jp.freestyles.mid.classes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DogOwner {
+    public static void main(String[] args) {
+
+        List<Dog> dogs = new ArrayList<>();
+        dogs.add(new Dog("山形賢", 7, "秋田犬"));
+        dogs.add(new Dog("山本一郎", 1, "プードル"));
+        dogs.add(new Dog("山田太朗", 2, "チワワ"));
+
+        for (Dog dog : dogs) {
+            System.out.printf("%s の %s は %d 歳です. %n",
+                dog.getDogType(), 
+                dog.getName(), 
+                dog.getAge());
+        }
+    }
+}
