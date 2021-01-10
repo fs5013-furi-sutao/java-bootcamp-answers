efb94d2007664fc16720fd3eb9423ac785d1ce5a
fs5013-furi-sutao <fs5013.furi.sutao@gmail.com>
2021-01-10 20:53:28 +0900

    Modified article

--- 04.mid/04.class-method/src/mid/classes/method/forfixing/CounterUser.java
+++ 04.mid/04.class-method/src/mid/classes/method/forfixing/CounterUser.java
@@ -2,9 +2,11 @@ package mid.classes.method.forfixing;
 
 public class CounterUser {
     
+    private static final int INSTANCE_NUMS = 6;
+
     public static void main(String[] args) {
         
-        Counter[] counters = new Counter[5];
+        Counter[] counters = new Counter[INSTANCE_NUMS];
 
         for (Counter counter : counters) {
             counter = new Counter();
