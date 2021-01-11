0c141ca31c960baeb797fe768e856aca138d8870
fs5013-furi-sutao <fs5013.furi.sutao@gmail.com>
2021-01-11 17:54:05 +0900

    Add article

---
+++ 04.mid/08-auto-generated-getter-setter-by-ide/src/mid/auto/ide/Division.java
@@ -0,0 +1,26 @@
+package mid.auto.ide;
+
+/**
+ * 部署クラス
+ */
+public class Division {
+    
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
