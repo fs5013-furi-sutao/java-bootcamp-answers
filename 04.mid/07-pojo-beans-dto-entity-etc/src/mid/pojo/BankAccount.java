1c4959861c7bf3b080c9c970150502f4405828d9
fs5013-furi-sutao <fs5013.furi.sutao@gmail.com>
2021-01-11 17:06:06 +0900

    Add article

---
+++ 04.mid/07-pojo-beans-dto-entity-etc/src/mid/pojo/BankAccount.java
@@ -0,0 +1,44 @@
+package mid.pojo;
+
+/**
+ * 銀行口座クラス
+ */
+public class BankAccount {
+    
+    private int id;
+    private String name;
+    private int age;
+    private double balance;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
