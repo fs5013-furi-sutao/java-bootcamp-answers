6e45a67334309b33048e08cb82a3c19363d96755
fs5013-furi-sutao <fs5013.furi.sutao@gmail.com>
2021-01-14 20:28:44 +0900

    Add article

---
+++ 05.advanced/03.super-keyword/src/advanced/superkey/entity/Dog.java
@@ -0,0 +1,20 @@
+package advanced.superkey.entity;
+
+public class Dog extends Animal {
+    
+    public Dog() {
+
+        super("[Dog] String 引数のコンストラクタを呼び出し");
+
+        System.out.format(
+                "[Dog] %s Dog クラスを生成しました. %n",
+                super.type
+            );
+        
+    }
+
+    @Override
+    public void bark(String sound) {
+        super.bark("わんわん" + sound);
+    }
+}
